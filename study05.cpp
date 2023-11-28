a62431614f57d452fc15b166a02f2091c7959e29
JaeHyunEo <jaehyun0803@naver.com>
2023-11-28 12:54:38 +0900

    cpp 자료형

---
+++ study05.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+double a = 1.23456789;
+int b = 12, c = 2;
+string str = "어벤져스";
+int main() {
+	printf("%.6lf\n", a);
+	printf("%03d\n", b);
+	printf("%03d\n", c);
+	printf("%s\n", str.c_str()); 
+	return 0;
+}
